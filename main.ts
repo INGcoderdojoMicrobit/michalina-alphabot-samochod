1f6408f861ba1f618001544bf942316f75071970
INGcoderdojoMicrobit <71919260+INGcoderdojoMicrobit@users.noreply.github.com>
2020-10-10 09:51:02 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "E") {
         AlphaBot2.RunDelay(Dir.forward, 50, 1)
+    } else if (receivedString == "C") {
+        AlphaBot2.RunDelay(Dir.backward, 50, 1)
+    } else if (receivedString == "D") {
+        AlphaBot2.RunDelay(Dir.turnRight, 50, 1)
+    } else if (receivedString == "F") {
+        AlphaBot2.RunDelay(Dir.turnLeft, 50, 1)
     }
 })
 basic.showIcon(IconNames.Asleep)
