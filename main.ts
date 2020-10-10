0d1d564226539d5b0268199d96391ef394da5151
INGcoderdojoMicrobit <71919260+INGcoderdojoMicrobit@users.noreply.github.com>
2020-10-10 09:45:29 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "E") {
+        AlphaBot2.RunDelay(Dir.forward, 50, 1)
+    }
+})
 basic.showIcon(IconNames.Asleep)
 radio.setGroup(123)
 basic.forever(function () {
